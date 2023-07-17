54b265d2e8a649c0f861a522628d8f8b43bf2629
Dudeiebot <ojoexcel5@gmail.com>
2023-07-17 23:41:37 +0100

    new ts working asapu

--- tests/staking_shii.ts
+++ tests/staking_shii.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { StakingShii } from "../target/types/staking_shii";
+import * as token from "@solana/spl-token";
 
 describe("staking_shii", () => {
   // Configure the client to use the local cluster.
@@ -8,30 +9,100 @@ describe("staking_shii", () => {
 
   const program = anchor.workspace.StakingShii as Program<StakingShii>;
   const provider = anchor.getProvider();
-  const wallet = anchor.web3.workspace.StakingShii.provider.wallet as anchor.web3.Keypair;
+  const wallet = anchor.workspace.StakingShii.provider.wallet as anchor.web3.Keypair;
 
-  let stakeInfoAccount = anchor.web3.PublicKey
-  let userInfoAccount = anchor.web3.PublicKey
-  let StakeInfoTokenAccount = anchor.web3.PublicKey
-  let mint = anchor.web3.PublicKey
-  let UserInfoTokenAccount = anchor.web3.PublicKey
+  let mintAccount: anchor.web3.PublicKey
+  let userTokenAccount = anchor.web3.PublicKey
+
+
+  let stakeInfoAccount: anchor.web3.PublicKey
+  let userInfoAccount: anchor.web3.PublicKey
+  let stakeInfoTokenAccount: anchor.web3.PublicKey
 
 
   before(async () => {
-   
-    userInfoAccount = anchor.web3.PublicKey.createProgramAddressSync(
-      [Buffer.from("USER"), provider.publicKey.toBuffer()],
-      program.programId
-    )
 
-    stakeInfoAccount = anchor.web3.PublicKey.createProgramAddressSync(
-      [Buffer.from("STAKE"), provider.publicKey.toBuffer(), mint.toBuffer()],
+    mintAccount: await token.CreateMint( //We created a mint account to mint and check for)
+      provider.connection,
+      wallet,
+      wallet.publicKey,
+      wallet.publicKey,
+      0
+    );
+    const tokenAccount = await token.getOrCreatedAssociatedTokenAccount(
+      provider.connection,
+      wallet,
+      mintAccount,
+      wallet.publicKey,
+    );
+    userTokenAccount = tokenAccount.address;
+    const mintTx = await token.mintTo(
+      provider.connection,
+      wallet,
+      mintAccount,
+      userTokenAccount,
+      wallet,
+      1
+    );
+
+    console.log("minted! ", mintTx)
+
+   
+    userInfoAccount = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("USER"), wallet.publicKey.toBuffer()],
       program.programId
-    )
+    )[0]
+
+    stakeInfoAccount = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("STAKE"), wallet.publicKey.toBuffer(), mintAccount.toBuffer()],
+      program.programId
+    )[0]
+
+    stakeInfoTokenAccount = token.getAssociatedTokenAddressSync(
+      mintAccount,
+      stakeInfoAccount,
+      true
+    );
 
   })
 
   it("Stake", async () => {
+
+    const tokenAccountBefore = await token.getAccount(
+      provider.connection,
+      userTokenAccount,
+    );
+
+    console.log("Before staking balance: ", tokenAccountBefore.amount)
+
+    const tx = program.methods.stake().accounts({
+      userInfo: userInfoAccount,
+      stakeInfo: stakeInfoAccount,
+      mint: mintAccount,
+      stakeTokenAccount: stakeInfoTokenAccount,
+      userTokenAccount: userTokenAccount,
+      owner: wallet.publicKey,
+      systemProgram:  anchor.web3.SystemProgram.programId,
+      tokenProgram: token.TOKEN_PROGRAM_ID,
+      associatedTokenProgram: token.ASSOCIATED_TOKEN_PROGRAM,
+      rent: anchor.web3.SYSVAR_RENT_PUBKEY
+    }).rpc()
+
+    const tokenAccountAfter = await token.getAccount(
+      provider.connection,
+      userTokenAccount
+    );
+    const stakeTokenAccountAfter = await token.getAccount(
+      provider.connection,
+      stakeInfoTokenAccount
+    );
+    const stakeInfo = await program.account.stakeInfo.fetch(
+      stakeInfoAccount
+    )
+    console.log(stakeInfo);
+
+    console.log("After Staking Balance (user) = ", tokenAccountAfter.amount)
+    console.log("After Staking Balance (staking) = ", stakeTokenAccountAfter.amount)
    
   });
 
